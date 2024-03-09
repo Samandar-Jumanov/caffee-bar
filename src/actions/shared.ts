4af8aba930e15e8f0edcf4b84bd55d2a85a0957c
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-03-08 20:11:48 -0800

    adding server actions

---
+++ src/actions/shared.ts
@@ -0,0 +1,74 @@
+"use server"
+import prisma from "../../prisma/prisma"
+
+export const getAllShared = async ( ) =>{
+      const getAllShared = await prisma.shared.findMany({
+          include : {
+              user : true
+          }
+      });
+
+      return getAllShared;
+
+}
+
+export const getSharedById = async (id: string) =>{
+    const getSharedById = await prisma.shared.findUnique({
+        where : {
+            id : id
+        },
+        include : {
+            user : true
+        }
+    });
+
+    return getSharedById;
+}
+
+
+
+
+export const createShared = async ( formData : FormData , ingredients : string[] , userId : string ) =>{
+      const user = await prisma.user.findUnique({
+          where : { id : userId },
+          include : { shared : true}
+      });
+
+      if(!user) {
+           return { message :"User not found | Something went wrong "}
+      }
+
+
+     const title = formData.get('title');
+     const description = formData.get('description');
+
+        const shared = await prisma.shared.create({
+            data : {
+                title : title as string,
+                description : description as string,
+                user : {
+                    connect : {
+                        id : userId
+                    }
+                }
+            }
+        });
+
+        if(!shared) {
+            return { message : "Shared not created | Something went wrong"}
+        }
+
+        await prisma.user.update({
+              where : { id : userId },
+              data : {
+                  shared : {
+                      connect : {
+                          id : shared.id
+                      }
+                  }
+              }
+        })
+        return { message : "Shared created successfully" }
+}
+
+

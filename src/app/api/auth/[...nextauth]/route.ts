d82a4747a8c41a575a73928266d2cb2893a73e16
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-03-13 18:19:24 -0700

    syntax error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ const authOptions  : AuthOptions    = {
           GithubProvider({
             clientId : process.env.GITHUB_CLIENT_ID  as string ,
             clientSecret :process.env.GITHUB_CLIENT_SECRET as string 
-          })
+          }),
           CredientialsProvider({
               name  : "credientials",
               credentials : {

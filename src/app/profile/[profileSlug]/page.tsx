306608adae8dc698361067ebacd33a6713afe062
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-03-16 02:59:59 -0700

    fixing bugs

--- src/app/profile/[profileSlug]/page.tsx
+++ src/app/profile/[profileSlug]/page.tsx
@@ -2,28 +2,28 @@
 
 import React from 'react';
 import { Container, Button } from '@mui/material';
-import { useSession } from "next-auth/react"
-import { UserAccount } from "@/components/userAccount"
+import { useSession } from "next-auth/react";
+import { UserAccount } from "@/components/userAccount";
 
-const UserAccountPage = async ({ params }: any) => {
-   const { data : session } = useSession()
-   const userName = params.profileSlug
-
-
-  if (!session?.user) {
-    return (
-      <Container sx={{
-        textAlign: 'center', mt: '-2px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
-      }}>
-        <Button href="/create-account" variant="contained" sx={{ mt: 2 }}>
-          To continue please , sign in 
-        </Button>
-      </Container>
-    );
-  }
+// Removed async since React components shouldn't be async
+const UserAccountPage = ({ params } : any ) => {
+  const { data: session } = useSession();
+  const userName = params.profileSlug;
 
   return (
-         <UserAccount userName={userName} />
+    <div>
+      {!session?.user ? (
+        <Container sx={{
+          textAlign: 'center', mt: '-2px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+        }}>
+          <Button href="/create-account" variant="contained" sx={{ mt: 2 }}>
+            To continue please, sign in
+          </Button>
+        </Container>
+      ) : (
+        <UserAccount userName={userName} />
+      )}
+    </div>
   );
 };
 

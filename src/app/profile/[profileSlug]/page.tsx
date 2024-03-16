7d6f6eb29ee0d62f184861d3b463e54420caabd7
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-03-15 22:43:56 -0700

    updating styling

--- src/app/profile/[profileSlug]/page.tsx
+++ src/app/profile/[profileSlug]/page.tsx
@@ -11,17 +11,19 @@ const UserAccountPage = async  ({ params } : any ) => {
   if (!user) {
     return (
       <Container sx={{
-        color: 'white', textAlign: 'center', mt: '-2px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+        textAlign: 'center', mt: '-2px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
       }}>
-        <Typography variant="h6">User not found</Typography>
-        <Button href="/create-account" variant="contained" sx={{ mt: 2 }}>Please sign in to your account</Button>
+        <Typography variant="h6"> We are unable to find a user with this username  . Please sign in to different  account</Typography>
+        <Button href="/create-account" variant="contained" sx={{ mt: 2 }}> 
+            Sign in 
+        </Button>
       </Container>
     );
   }
 
   return (
     <Container maxWidth="lg" sx={{
-      mt: 4, color: 'white', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+      mt: 4, height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
     }}>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
         <Grid item xs={12} md={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>

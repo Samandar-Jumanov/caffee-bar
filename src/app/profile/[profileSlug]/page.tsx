d9342622074eb5ac054659056ce35b26dd6bf720
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-03-16 04:18:40 -0700

    fixing styling

--- src/app/profile/[profileSlug]/page.tsx
+++ src/app/profile/[profileSlug]/page.tsx
@@ -1,29 +1,51 @@
-"use client"
 
 import React from 'react';
-import { Container, Button } from '@mui/material';
-import { useSession } from "next-auth/react";
-import { UserAccount } from "@/components/userAccount";
+// import { useSession } from "next-auth/react";
+import { getUserData }  from "../../../actions/user"
+import { Container, Grid, Typography, Avatar, Paper, Box, Button , Divider } from '@mui/material';
+import Image from "next/image";
+import { IUser, ISharedCoffe } from "@/types/types";
 
-// Removed async since React components shouldn't be async
-const UserAccountPage = ({ params } : any ) => {
-  const { data: session } = useSession();
+const UserAccountPage = async  ({ params } : any ) => {
   const userName = params.profileSlug;
+  const user :IUser | undefined  | any   = await getUserData(userName);
 
+ 
   return (
-    <div>
-      {!session?.user ? (
-        <Container sx={{
-          textAlign: 'center', mt: '-2px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
-        }}>
-          <Button href="/create-account" variant="contained" sx={{ mt: 2 }}>
-            To continue please, sign in
-          </Button>
-        </Container>
-      ) : (
-        <UserAccount userName={userName} />
-      )}
-    </div>
+<Container maxWidth="lg" sx={{
+      mt: '-30px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+    }}>
+      <Box sx={{ textAlign: 'center', mb: 4 }}>
+        <Avatar src={user?.image || undefined} alt={user?.name || ""} sx={{ width: 90, height: 90, mb: 2, mx: 'auto' }} />
+        <Typography variant="h6">{user?.name}</Typography>
+        <Typography variant="body1">{user?.email}</Typography>
+      </Box>
+      <Grid container spacing={2} justifyContent="center">
+        {user?.shared && user?.shared.length === 0 ? (
+           <Box sx={{ display: 'block', textAlign: 'center' }}>
+           <Typography>You do not have any created ingredients.</Typography>
+           <Divider sx={{ my: 2 }} /> 
+           <Box sx={{ mt: 2 }}> 
+             <Button href="/add-coffee" color="warning" variant="contained">Create a special ingredient</Button>
+           </Box>
+         </Box>
+        ) : (
+          user?.shared?.map((item: ISharedCoffe) => (
+            <Grid item xs={12} sm={6} md={4} key={item.id}>
+              <Paper elevation={3} sx={{ p: 2, mb: 2, bgcolor: '#424242', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                <Typography variant="h5" sx={{ textAlign: 'center' }}>{item.title}</Typography>
+                <Typography variant="body1" sx={{ mt: 1, textAlign: 'center' }}>{item.description}</Typography>
+                {item.image && (
+                  <Box sx={{ maxWidth: '100%', mt: 2, borderRadius: '4px', overflow: 'hidden' }}>
+                    <Image src={item.image} alt={item.title} width={200} height={200} layout="intrinsic" />
+                  </Box>
+                )}
+              </Paper>
+            </Grid>
+          ))
+        )}
+      </Grid>
+    </Container>
   );
 };
 
